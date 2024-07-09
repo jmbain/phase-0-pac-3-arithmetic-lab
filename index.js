7b54fee476017495794fc41e8ee2290a33c59810
Josh Bain <joshuambain@gmail.com>
2024-07-08 20:57:28 -0400

    Complete arithmetic lab

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a,b) {
+    return a + b;
+}
+function subtract(a,b) {
+    return a - b;
+}
+function multiply(a,b) {
+    return a * b;
+}
+function divide(a,b) {
+    return a / b;
+}
+//Reset number... and simplify index file
+number = 270
+function increment(number){
+    return (number += 1);
+}
+function decrement(number){
+    return (number -= 1);
+}
+// console.log(parseInt("2.2222", 10));
+function makeInt(number) {
+    return parseInt(number, 10);
+}
+function preserveDecimal(number) {
+    return parseFloat(number);
+}
